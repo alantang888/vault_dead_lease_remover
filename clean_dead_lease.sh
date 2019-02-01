7628fe8bd1d7f964b37bf297c498e99718d7e50c
Alan Tang <alan.tang@lalamove.com>
2019-02-01 18:03:56 +0800

    Remove no needed /

--- clean_dead_lease.sh
+++ clean_dead_lease.sh
@@ -32,9 +32,9 @@ do
     # Check TTL
     TTL=$(curl -s -k --header "X-Vault-Token: $X_VAULT_TOKEN" -d "{\"lease_id\": \"$LEASE_PREFIX$LEASE\"}" -X PUT $VAULT_ENDPOINT/v1/sys/leases/lookup |jq ".data.ttl")
     if (( $TTL < -$KILL_SECOND )); then
-      echo "$LEASE_PREFIX/$LEASE --> $TTL Will force revoke"
+      echo "$LEASE_PREFIX$LEASE --> $TTL Will force revoke"
       # Forec expire lease
-      curl -s -k --header "X-Vault-Token: $X_VAULT_TOKEN" -X PUT $VAULT_ENDPOINT/v1/sys/leases/revoke-force/$LEASE_PREFIX/$LEASE
+      curl -s -k --header "X-Vault-Token: $X_VAULT_TOKEN" -X PUT $VAULT_ENDPOINT/v1/sys/leases/revoke-force/$LEASE_PREFIX$LEASE
     fi
   done
 done
